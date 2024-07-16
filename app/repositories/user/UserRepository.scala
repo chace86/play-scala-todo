4b67bd9e5caf528466e09d3ddfeb90b93620f134
Chace Anderson <chace.5008@gmail.com>
2024-07-16 00:19:09 -0400

    update user action

--- app/repositories/user/UserRepository.scala
+++ app/repositories/user/UserRepository.scala
@@ -5,13 +5,13 @@ import scala.collection.mutable
 case class User(username: String, password: String)
 
 class UserRepository {
-  
-    // TODO: replace with database
-    private val users: mutable.Map[String, User] = mutable.Map()
 
-    def getUser(username: String): Option[User] = users.get(username)
+  // TODO: replace with database
+  private val users: mutable.Map[String, User] = mutable.Map()
 
-    def createUser(username: String, password: String): Boolean = {
-        !users.contains(username) && users.put(username, User(username, password)).isDefined
-    }
+  def getUser(username: String): Option[User] = users.get(username)
+
+  def createUser(username: String, password: String): Boolean = {
+    !users.contains(username) && users.put(username, User(username, password)).isDefined
+  }
 }
